b561cb30c70c9a6db21d15973b7691a81952776d
Muhammad Ahmad <213047@std.aumc.edu.pk>
2023-10-23 11:20:51 +0500

    Lab 6

---
+++ Examples/E_1/E_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 98, b = 0;
+            double result = 0;
+            try
+            {
+                result = SafeDivision(a, b);
+                Console.WriteLine($"{a} divided by {b} = {result}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Attempted divide by zero.");
+            }
+        }
+
+        static double SafeDivision(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return x / y;
+        }
+    }
+}
---
+++ Examples/E_3/E_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class WorkItem
+{
+    public static void Main(string[] args)
+    {
+        // Example usage in the Main method
+        WorkItem workItem = new WorkItem("Sample Work", "Description of the work", TimeSpan.FromHours(2));
+        Console.WriteLine(workItem.ToString());
+    }
+
+    private static int currentId = 0;
+
+    protected int Id { get; set; }
+    protected string Title { get; set; }
+    protected string Description { get; set; }
+    protected TimeSpan JobLength { get; set; }
+
+    public WorkItem()
+    {
+        Id = GetNextId();
+        Title = "Default Title";
+        Description = "Default Description";
+        JobLength = TimeSpan.Zero;
+    }
+
+    public WorkItem(string title, string desc, TimeSpan jobLen)
+    {
+        Id = GetNextId();
+        Title = title;
+        Description = desc;
+        JobLength = jobLen;
+    }
+
+    static WorkItem()
+    {
+        currentId = 0;
+    }
+
+    protected int GetNextId()
+    {
+        return ++currentId;
+    }
+
+    public void Update(string title, TimeSpan jobLen)
+    {
+        Title = title;
+        JobLength = jobLen;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0} - {1}", Id, Title);
+    }
+}
